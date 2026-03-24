c5ab46b4bece79cfa8fd742e06dd6756f7280946
Daria Shilnikova <girl.is.anime20@gmail.com>
2026-03-24 08:49:55 +0000

    feat: add pagination

--- my-app/src/components/table/table.tsx
+++ my-app/src/components/table/table.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import type { ModuleCheckResult } from '../../types/types';
 import './table.css';
 
@@ -6,7 +6,23 @@ interface ResultTableProps {
   results: ModuleCheckResult[];
 }
 
+const RESULTS_PER_PAGE = 10;
+
 const ResultTable: React.FC<ResultTableProps> = ({ results }) => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+
+   useEffect(() => {
+    setCurrentPage(1);
+  }, [results]);
+
+
+  const totalPages = Math.ceil(results.length / RESULTS_PER_PAGE);
+  const currentResults = results.slice(
+    (currentPage - 1) * RESULTS_PER_PAGE,
+    currentPage * RESULTS_PER_PAGE
+  );
+
+
   return (
     <div className="result-table-wrapper">
       <table className="result-table">
@@ -19,7 +35,7 @@ const ResultTable: React.FC<ResultTableProps> = ({ results }) => {
           </tr>
         </thead>
         <tbody>
-          {results.map((r, idx) => (
+          {currentResults.map((r, idx) => (
             <tr key={idx}>
               <td>{r.moduleName}</td>
               <td>{r.item}</td>
@@ -29,6 +45,25 @@ const ResultTable: React.FC<ResultTableProps> = ({ results }) => {
           ))}
         </tbody>
       </table>
+ {totalPages > 1 && (
+        <div className="pagination">
+          <button
+            onClick={() => setCurrentPage((p) => Math.max(p - 1, 1))}
+            disabled={currentPage === 1}
+          >
+            Назад
+          </button>
+          <span>
+            {currentPage} / {totalPages}
+          </span>
+          <button
+            onClick={() => setCurrentPage((p) => Math.min(p + 1, totalPages))}
+            disabled={currentPage === totalPages}
+          >
+            Вперед
+          </button>
+        </div>
+      )}
     </div>
   );
 };
